125276b52f919f52b0b69dea7ec9a28dc1ef4ad2
Anibal Copitan <github@pprios.com>
2022-08-11 22:57:40 -0500

    Create tool to convert from text to image with SEO

---
+++ README.md
@@ -0,0 +1,18 @@
+# De Texto a Imagen SEO
+
+Tool useful for generate thumbnail(400x277) for your blog or another utility
+just need to set the text in the variable into the file `main.py`
+
+`astr` = De texto a imagen seo generator
+
+## Requirement
+ * python 3 
+
+## Execute
+
+	cd <MainDirectory>
+	pip install
+	python main.py
+
+## Example
+this tool is use into the blog: https://blog.anibalcopitan.com
---
+++ main.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+from slugify import slugify
+
+# img = Image.new('RGB', (400, 277), color='black')
+# d = ImageDraw.Draw(img)
+# font = ImageFont.truetype('./assets/Helvetica-Font/Helvetica.ttf', 40)
+# d.text((30, 30), "Basic programs to install on raspberry by terminal", fill=(255, 255, 255), font=font)
+#
+astr = '''De texto a imagen seo generator'''
+para = textwrap.wrap(astr, width=25)
+
+MAX_W, MAX_H = 400, 277
+im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
+draw = ImageDraw.Draw(im)
+font = ImageFont.truetype(
+    './assets/Helvetica-Font/Helvetica.ttf', 30)
+
+current_h, pad = 70, 10
+for line in para:
+    w, h = draw.textsize(line, font=font)
+    # w, h = draw.textlength(line, font=font)
+    draw.text(((MAX_W - w) / 2, current_h), line, font=font)
+    current_h += h + pad
+#
+file_name = slugify(astr)
+im.save(file_name + '-400x277.png')
